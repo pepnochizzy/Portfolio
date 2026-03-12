710227cce240ba86f2d365f2fc697dede13b4fe1
pepnochizzy <laragray@live.co.uk>
2026-03-12 16:29:40 +0000

    feat: adding project components, will add dynamic page for in-depth discussion

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import AboutMe from "@/components/AboutMe";
+import Projects from "@/components/Projects";
 import TechStack from "@/components/TechStack";
 
 export default function Home() {
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,1 +1,22 @@
-export default function Projects() {}
+import Image from "next/image";
+
+export default function Projects({
+  src,
+  alt,
+  size,
+  projectName,
+  projectDesc,
+  href,
+  stack,
+}) {
+  return (
+    <a className="flex flex-row" href={href}>
+      <Image src={src} alt={alt} width={size} />
+      <div className="flex flex-row">
+        <h2>{projectName}</h2>
+        <h3>{stack}</h3>
+        <p>{projectDesc}</p>
+      </div>
+    </a>
+  );
+}
